953caa1df847e18e368700f8a6f70674dae52dfd
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2020-01-29 09:09:57 +0100

    Add fuse_main function which is a macro in the fuse header

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,17 @@
 #![allow(non_upper_case_globals)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+use std::os::raw::*;
+
+/// Main function of FUSE
+///
+/// Implemented as a macro in the original fuse header.
+pub unsafe fn fuse_main(
+    argc: c_int,
+    argv: *mut *mut c_char,
+    op: *const fuse_operations,
+    user_data: *mut c_void,
+) -> c_int {
+    fuse_main_real(argc, argv, op, std::mem::size_of_val(&*op), user_data)
+}
